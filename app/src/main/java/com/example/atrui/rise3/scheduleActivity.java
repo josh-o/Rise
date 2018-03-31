ea6f58b25a5a92dc325bec5d869cb5d2765e0676
JoshRise <headhunterisftl@gmail.com>
2018-03-31 16:09:50 -0400

    test

--- app/src/main/java/com/example/atrui/rise3/scheduleActivity.java
+++ app/src/main/java/com/example/atrui/rise3/scheduleActivity.java
@@ -64,6 +64,7 @@ public class scheduleActivity extends AppCompatActivity {
                         //textView2.setText(date);
                         textView2.setText(rs.getString(2));
                     }
+                    //test
 
 
                 } catch (ClassNotFoundException e) {
