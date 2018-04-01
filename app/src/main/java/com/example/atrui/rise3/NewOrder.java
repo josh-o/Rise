5fa2f00d6c38d1846fd1de620dd511eeaf008d0d
atruitt <atruitt20@gmail.com>
2018-04-01 18:05:53 -0400

    updated order activity popup

--- app/src/main/java/com/example/atrui/rise3/NewOrder.java
+++ app/src/main/java/com/example/atrui/rise3/NewOrder.java
@@ -1,17 +1,28 @@
 package com.example.atrui.rise3;
 
+
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.PopupWindow;
 import android.view.ViewGroup.LayoutParams;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 
 /**
@@ -20,6 +31,7 @@ import android.view.ViewGroup.LayoutParams;
 
 public class NewOrder extends Fragment{
 
+    Dialog createItemDialog;
     ImageButton lightRoast;
     String lightRoast1 = "Light Roast";
     Button checkOut;
@@ -31,14 +43,13 @@ public class NewOrder extends Fragment{
         lightRoast = (ImageButton) view.findViewById(R.id.LightRoastButton2);
 
     //    String modifyTest = getText().toString();
-        lightRoast.setOnClickListener(new View.OnClickListener(){
+    /*    lightRoast.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                TextView orderLine1 = (TextView) view.findViewById(R.id.orderLine1);
-                orderLine1.setText(lightRoast1);
+
             }
         });
-
+    */
         checkOut = (Button) view.findViewById(R.id.checkoutButton);
 
         checkOut.setOnClickListener(new View.OnClickListener(){
@@ -48,8 +59,35 @@ public class NewOrder extends Fragment{
 
             }
         });
-        return view;
 
+        return view;
+    }
+
+
+    public void createNewItemPopup2(View v){
+        createItemDialog.setContentView(R.layout.popup_layout);
+        ImageButton closeDialog = (ImageButton)createItemDialog.findViewById(R.id.closeDialog);
+        closeDialog.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                createItemDialog.dismiss();
+            }
+        });
+        //Create new item logic
+        Button createItem = (Button)createItemDialog.findViewById(R.id.createItem);
+        createItem.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+
+
+                createItemDialog.dismiss();
+                ////displayTable();
+            }
+        });
+        createItemDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        createItemDialog.show();
 
     }
 
